81bc6804c6b197155baeac0a90e42edfd231da4a
David Sobíšek <dsobisek04@gmail.com>
2026-01-02 17:51:59 +0100

    Create transaction

--- src/abl/TransactionService.kts
+++ src/abl/TransactionService.kts
@@ -2,8 +2,14 @@
 class TransactionService()
 {
 
-    fun createTransaction()
-    {
+    fun createTransaction(amount: Int,name:String) {
+        if (amount <= 0) {
+            throw IllegalArgumentException("Amount must be greater than 0")
+        }
+        if(name.length<=0)
+        {
+            throw IllegalArgumentException("Name cant be nullable")
+        }
 
     }
     fun removeTransaction()
