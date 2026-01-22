75a5ca6223314b6fcd30f5fad43ad707904281c5
David Sobíšek <dsobisek04@gmail.com>
2026-01-22 22:05:09 +0100

    Rozpracovaní UI

--- app/src/main/java/com/example/finamobileapp/MainActivity.kt
+++ app/src/main/java/com/example/finamobileapp/MainActivity.kt
@@ -3,45 +3,59 @@ package com.example.finamobileapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.finamobileapp.ui.theme.FinaMobileAppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            FinaMobileAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Dashboard()
         }
     }
 }
 
+
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Dashboard() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFA69D9D)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text("Income", fontSize = 32.sp, fontWeight = FontWeight.Normal)
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text("Expenses", fontSize = 32.sp, fontWeight = FontWeight.Normal)
+
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text("Sumarize", fontSize = 28.sp)
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    FinaMobileAppTheme {
-        Greeting("Android")
-    }
+fun DashboardPreview() {
+    Dashboard()
 }
